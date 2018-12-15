d8acf07f498c9ad02667458af4a2e87387e2f67a
“用wangkai <l>
2018-12-15 14:02:48 +0800

    二叉树的遍历

---
+++ 94_Binary Tree Inorder Traversal/94_Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+//µÝ¹é°æ
+vector<int> inorderTraversal(TreeNode* root)
+{
+	vector<int> result;
+	inorderTraversalCore(root, result);
+	return result;
+}
+void inorderTraversalCore(TreeNode* root, vector<int> &v)
+{
+	if (root == nullptr)
+		return;
+	inorderTraversalCore(root->left, v);
+	v.push_back(root->val);
+	inorderTraversalCore(root->right, v);
+}
+//·ÇµÝ¹é°æ
+vector<int> inorderTraversal2(TreeNode* root)
+{
+	vector<int> result;
+	if (root != nullptr)
+	{
+		stack<TreeNode*>st;
+		while (!st.empty() || root != nullptr)
+		{
+			if (root != nullptr)
+			{
+				st.push(root);
+				root = root->left;
+			}
+			else
+			{
+				root = st.top();
+				result.push_back(root->val);
+				st.pop();
+				root = root->right;
+			}
+		}
+	}
+	return result;
+}
+int main(void)
+{
+	system("pause");
+	return 0;                                              
+}

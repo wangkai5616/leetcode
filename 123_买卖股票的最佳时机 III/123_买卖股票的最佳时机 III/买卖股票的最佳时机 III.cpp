5206c66663a15cfca38fcff82931255ceb8b1b1b
“用wangkai <l>
2019-01-10 20:18:54 +0800

    动态规划

--- 123_买卖股票的最佳时机 III/123_买卖股票的最佳时机 III/买卖股票的最佳时机 III.cpp
+++ 123_买卖股票的最佳时机 III/123_买卖股票的最佳时机 III/买卖股票的最佳时机 III.cpp
@@ -2,6 +2,12 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+/*
+统计第i天之前卖出的最大利润和第i天之后买入的最大利润，
+然后看哪个分界点的最大利润最大
+因为两笔交易肯定不能交错，比如第1天买入，第3天卖出
+和第2天买入，第5天卖出，不能这样交错
+*/
 int maxProfit(vector<int>& prices)
 {
 	if (prices.size() < 2)
@@ -11,17 +17,19 @@ int maxProfit(vector<int>& prices)
 	vector<int> v2(n, 0);
 	int min_value = prices[0];
 	int max_value = prices[n - 1];
-	//从前往后记录第i天的最大利润
+	//第i天之前卖出的最大利润
 	for (int i = 1; i < n; ++i)
 	{
 		min_value = min(min_value, prices[i]);
 		v1[i] = max(v1[i - 1], prices[i] - min_value);
 	}
+	//第i天之后买入的最大利润
 	for (int i = n - 2; i >= 0; --i)
 	{
 		max_value = max(max_value, prices[i]);
 		v2[i] = max(v2[i], max_value - prices[i]);
 	}
+	//统计哪一天的最大利润最大
 	int max_profit = 0;
 	for (int i = 0; i < n; ++i)
 		max_profit = (max_profit, v1[i] + v2[i]);

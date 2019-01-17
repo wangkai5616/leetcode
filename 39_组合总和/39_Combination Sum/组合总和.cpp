96d93258ca0f2346514acea3b3671babe16f2adc
“用wangkai <l>
2019-01-17 14:54:49 +0800

    深度优先探索

--- 39_组合总和/39_Combination Sum/组合总和.cpp
+++ 39_组合总和/39_Combination Sum/组合总和.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+/*
+给定一个无重复元素的数组 candidates 和一个目标数 target ，
+找出 candidates 中所有可以使数字和为 target 的组合。
+candidates 中的数字可以无限制重复被选取。
+说明：
+所有数字（包括 target）都是正整数。
+解集不能包含重复的组合。
+示例 1:
+
+输入: candidates = [2,3,6,7], target = 7,
+所求解集为:
+[[7],[2,2,3]]
+*/
 void BackTrace(vector<vector<int>> &result, vector<int> &v, vector<int> candidates,
 	int target, int remain, int begin);
 vector<vector<int>> combinationSum(vector<int>& candidates, int target)

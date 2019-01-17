c9a1b8110e75d9e85bc28671f6e68010a9bc0932
“用wangkai <l>
2019-01-17 15:46:16 +0800

    深度优先探索

--- 46_全排列/46_Permutations/全排列.cpp
+++ 46_全排列/46_Permutations/全排列.cpp
@@ -5,6 +5,10 @@ using namespace std;
 /*
 给定一个没有重复数字的序列，返回其所有可能的全排列。
 */
+/*
+此题较特殊，因为涉及从前向后，还要从后向前，所以不能根据下标
+是否到达num.size()做判断
+*/
 vector<vector<int>> permute(vector<int>& nums)
 {
 	vector<vector<int>> result;

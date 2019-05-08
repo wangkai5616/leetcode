8e33f93f219c194f019941d5d562c729e98fb178
“用wangkai <l>
2019-05-08 10:09:59 +0800

    字符串匹配问题

--- 44_通配符匹配/44_Wildcard Matching/Wildcard Matching.cpp
+++ 44_通配符匹配/44_Wildcard Matching/Wildcard Matching.cpp
@@ -24,6 +24,40 @@ bool isMatchCore(string s, int i, string p, int j)
 		return isMatchCore(s, i + 1, p, j + 1);
 	return false;
 }
+
+
+bool isMatch(string s, string p) {
+	if (s == p)
+		return true;
+	//jstar用来记p中'*'的位置, istar记录s中'*'所能匹配的字符的最终位置
+	int i = 0, j = 0, iStar = -1, jStar = -1;
+	while (i < s.size())
+	{
+		if (s[i] == p[j] || p[j] == '?')
+		{//达到了消除一个字符的条件
+			++i; ++j;
+		}
+		else if (p[j] == '*')
+		{//当前字符串为'*'，记录下来当前位置
+			iStar = i;
+			jStar = j++;
+		}
+		else if (jStar >= 0)
+		{//当两个字符无法匹配时，如果出现了'*'，那么用'*'号抵消，并且p中'*'位置不变
+			i = ++iStar;
+			j = jStar + 1;
+		}
+		//不满足匹配条件，并且没有出现'*'
+		else
+			return false;
+	}
+	//查找完字符串中所有的'*'
+	while (j < p.size() && p[j] == '*')
+		++j;
+	//所有的字符都得到了匹配
+	return j == p.size();
+}
+
 int main(void)
 {
 	string s = "adceb";

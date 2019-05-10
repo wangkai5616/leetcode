ed0a339db6c82de7097045f5bd391907bfa0d9fd
“用wangkai <l>
2019-05-10 15:51:47 +0800

    根据条件移除数组元素

--- 80_删除排序数组中的重复项 II/80_ Remove Duplicates From Sorted Array ii/Remove Duplicates From Sorted Array ii.cpp
+++ 80_删除排序数组中的重复项 II/80_ Remove Duplicates From Sorted Array ii/Remove Duplicates From Sorted Array ii.cpp
@@ -5,29 +5,33 @@ using namespace std;
 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
 */
-int removeDuplicates1(vector<int>& nums)
+int removeDuplicates(vector<int>& nums)
 {
-	if (nums.size() < 3)
-		return nums.size();
-	//如果数组中已经出现两次同样的元素，那么help=1
-	int help = 0;
-	for (auto iter = nums.begin() + 1; iter != nums.end();)
+	if (nums.size() == 0)
+		return 0;
+	int index = -1;
+	int length = 0;
+	int times = 0;
+	for (int i = 0; i < nums.size(); ++i)
 	{
-		if (*iter == *(iter - 1) && help == 1)
-			//这时这个方法的最大缺点，删除元素需要移动的元素较多，所以时间复杂度高
-			iter = nums.erase(iter);
-		else if (*iter == *(iter - 1) && help == 0)
+		if (i == 0 || nums[i] != nums[i - 1])
 		{
-			++iter;
-			help = 1;
+			times = 1;
+			nums[++index] = nums[i];
+			++length;
+
 		}
-		else
+		else if (nums[i] == nums[i - 1])
 		{
-			++iter;
-			help = 0;
+			if (times == 1)
+			{
+				++times;
+				nums[++index] = nums[i];
+				++length;
+			}
 		}
 	}
-	return nums.size();
+	return length;
 }
 
 //时间复杂度为O(n)，空间复杂度为O(1)的方法

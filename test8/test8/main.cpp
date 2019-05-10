57ec43edfb52873a653e379082d8aafec51fc81c
“用wangkai <l>
2019-05-10 16:02:56 +0800

    字符串

--- test8/test8/main.cpp
+++ test8/test8/main.cpp
@@ -1,20 +1,21 @@
 #include<iostream>
 #include<string>
 using namespace std;
-struct Test {
-	typedef int foo;
-};
+int strStr(string haystack, string needle)
+{
+	if (needle.size() == 0)
+		return 0;
+	if (haystack.size() == 0)
+		return -1;
+	for (int i = 0; i < haystack.size(); ++i)
+	{
 
-template <typename T>
-void f(typename T::foo) { cout << "hello" << endl; } // Definition #1
-
-template <typename T>
-void f(T) { cout << "hello" << endl; }               // Definition #2
+	}
+}
 
 int main(void)
 {
-	f<Test>(10); // Call #1.
-	f<int>(10);  // Call #2. Without error (even though there is no int::foo) thanks to SFINAE.
+
 	system("pause");
 	return 0;
 }

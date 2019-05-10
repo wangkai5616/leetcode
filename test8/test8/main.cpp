de47bbe816f8ddee54cd017261159a6c5e1628e6
“用wangkai <l>
2019-05-10 17:04:54 +0800

    栈

--- test8/test8/main.cpp
+++ test8/test8/main.cpp
@@ -1,22 +1,31 @@
 #include<iostream>
+#include<stack>
 #include<string>
+#include<algorithm>
 using namespace std;
-int divide(int dividend, int divisor)
+int longestValidParentheses(string s)
 {
-	long long a = dividend > 0 ? dividend : -(long long)dividend;
-	long long b = divisor > 0 ? dividend : -(long long)dividend;
-	long long result = 0;
-	while (a > b)
+	int curLength = 0;
+	int maxLength = 0;
+	stack<int> si;
+	int last = -1;
+	for (int i = 0; i < s.size(); ++i)
 	{
-		long long c = b;
-		for (int i = 0; a >= c; ++i, c << 1)
+		if (s[i] == '(')
+			si.push(i);
+		else
 		{
-			a -= c;
-			result += 1 << i;
+			if (si.empty())
+				last = i;
+			else
+			{
+				si.pop();
+				if(si.empty())
+					maxLength=
+			}
 		}
 	}
 }
-
 int main(void)
 {
 

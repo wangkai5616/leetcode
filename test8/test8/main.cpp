f9be9d905c79628dd049a63962c6eaa55da46eb0
“用wangkai <l>
2019-05-13 10:23:59 +0800

    滑动窗口

--- test8/test8/main.cpp
+++ test8/test8/main.cpp
@@ -5,30 +5,37 @@
 #include<algorithm>
 #include<memory>
 using namespace std;
-void setZeroes(vector<vector<int>>& matrix)
+string minWindow(string s, string t)
 {
-	int m = matrix.size();
-	int n = matrix[0].size();
-	vector<int> row(m, 0);
-	vector<int> col(n, 0);
-	for (int i = 0; i < m; ++i)
+	if (s.size() == 0 || t.size() == 0 || s.size() < t.size())
+		return "";
+	//用于保存s中出现字符串t中的某个字符的数量
+	int *appeared = new int[256]();
+	//用于保存字符串t中出现的某个字符的数量
+	int *expected = new int[256]();
+	for (int i = 0; i < t.size(); ++i)
+		expected[t[i]]++;
+	int minWidth = INT_MAX;
+	int minStart = 0;
+	int num = 0;
+	int winStrat = 0;
+	for (int winEnd = 0; winEnd < s.size(); ++winEnd)
 	{
-		for (int j = 0; j < n; ++j)
+		if (expected[s[winEnd]]>0)
 		{
-			if (matrix[i][j] == 0)
+			appeared[s[winEnd]]++;
+			if (appeared[s[winEnd]] <= expected[s[winEnd]])
+				++num;
+		}
+		if (num == t.size())
+		{
+			while (appeared[s[winStrat]]>expected[s[winStrat]])
 			{
-				row[i] = 1;
-				col[j] = 1;
+				appeared[s[winStrat]--];
+				++winStrat;
 			}
 		}
 	}
-	for (int i = 0; i < m; ++i)
-		for (int j = 0; j < n; ++j)
-		{
-			if (row[i] == 1 || col[j] == 1)
-				matrix[i][j] = 0;
-		}
-	return;
 }
 int main(void)
 {

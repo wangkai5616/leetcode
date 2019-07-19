c3cda4372c9cd934943f3ad1d04ccea366a3dd35
“用wangkai <l>
2019-07-19 20:20:04 +0800

    二分查找

--- 378_有序矩阵中第K小的元素/378_有序矩阵中第K小的元素/378_有序矩阵中第K小的元素.cpp
+++ 378_有序矩阵中第K小的元素/378_有序矩阵中第K小的元素/378_有序矩阵中第K小的元素.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<algorithm>
 using namespace std;
+/*
+解法一：将所有元素放入堆中进行查找
+*/
+int kthSmallest(vector<vector<int>>& matrix, int k)
+{
+	priority_queue<int> q;
+	for (int i = 0; i < matrix.size(); ++i)
+	{
+		for (int j = 0; j < matrix[i].size(); ++j)
+		{
+			q.emplace(matrix[i][j]);
+			if (q.size() > k)
+				q.pop();
+		}
+	}
+	return q.top();
+}
 /*
 思路：这题我们也可以用二分查找法来做，我们由于是有序矩阵，那么左上角的数字
 一定是最小的，而右下角的数字一定是最大的，所以这个是我们搜索的范围，然后我们

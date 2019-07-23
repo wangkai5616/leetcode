517e9f46c8539d0b4a3d6ecba7586318156ef59b
“用wangkai <l>
2019-07-23 10:08:19 +0800

    哈希表

---
+++ 242_有效的字母异位词/242_有效的字母异位词/242_有效的字母异位词.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+bool isAnagram(string s, string t)
+{
+	if (s.size() != t.size())
+		return false;
+	int m[26] = { 0 };
+	for (int i = 0; i < s.size(); ++i)
+		++m[s[i] - 'a'];
+	for (int j = 0; j < t.size(); ++j)
+		if (--m[t[j] - 'a'] < 0)
+			return false;
+	return true;
+}

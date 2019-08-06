e6b768672f0271f587961d08481f6d52ef3b822c
“用wangkai <l>
2019-08-06 09:34:19 +0800

    反转

--- 189_旋转数组/189_旋转数组/189_旋转数组.cpp
+++ 189_旋转数组/189_旋转数组/189_旋转数组.cpp
@@ -6,7 +6,7 @@ void rotate(vector<int>& nums, int k)
 	if (nums.empty() || k%nums.size() == 0)
 		return;
 	int n = nums.size();
-	reverse(nums.begin(), nums.begin() + n - k);
+	reverse(nums.begin(), nums.begin() + n - k);//reverse不包含最后一个元素
 	reverse(nums.begin() + n - k, nums.end());
 	reverse(nums.begin(), nums.end());
 	return;
